f08f9022b4fd0c4df03c7e2c886e79fbe8bc398a
lmatkows <lmatkows@student.42perpignan.fr>
2025-03-28 17:14:02 +0100

    initialisation

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 13:57:45 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/03/27 13:59:44 by lmatkows         ###   ########.fr       */
+/*   Updated: 2025/03/28 17:07:32 by lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,62 @@
 # include <stdio.h>
 # include "libft.h"
 # include "get_next_line.h"
+# include "../minilibx/mlx.h"
+
+typedef struct s_pix
+{
+	struct s_var	*var;
+	int				x;
+	int				y;
+	int				z;
+	unsigned int	col;
+}	t_pix;
+
+// typedef struct s_line
+// {
+// 	t_pix			pix_1;
+// 	t_pix			pix_2;
+// }	t_line;
+
+typedef struct s_img
+{
+	struct s_var	*var;
+	void			*img_ptr;
+	char			*data_addr;
+	char			*path;
+	int				bit_per_pix;
+	int				line_len;
+	int				endian;
+	int				width;
+	int				height;
+}	t_img;
+
+typedef struct s_textures
+{
+	struct s_var	*var;
+	t_img			NO_img;
+	t_img			SO_img;
+	t_img			WE_img;
+	t_img			EA_img;
+	unsigned int	FL_col;
+	unsigned int	CE_col;
+}	t_textures;
+
+typedef struct s_win
+{
+	struct s_var	*var;
+	void			*win_ptr;
+	char			*title;
+	int				width;
+	int				height;
+}	t_win;
+
+typedef struct s_var
+{
+	void			*mlx_ptr;
+	int				status;
+	t_win			win;
+	t_textures		txtr;
+}	t_var;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,48 @@
 /*   By: lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 13:55:36 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/03/28 13:10:52 by lmatkows         ###   ########.fr       */
+/*   Updated: 2025/03/28 17:11:20 by lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	main(void)
+t_img	*ft_import_xpm_img(t_var *var, char *path, t_img *img)
 {
+	(void)var;
+	img->data_addr = mlx_xpm_file_to_image(var->mlx_ptr, path, &img->width, &img->height);
+	if (!img->data_addr)
+		return (NULL);
+	return (img);
+}
+
+t_textures *ft_init_textures(t_var *var)
+{
+	if (!ft_import_xpm_img(var, var->txtr.SO_img.path, &var->txtr.SO_img))
+		return (NULL);
+	if (!ft_import_xpm_img(var, var->txtr.WE_img.path, &var->txtr.WE_img))
+		return (NULL);
+	if (!ft_import_xpm_img(var, var->txtr.EA_img.path, &var->txtr.EA_img))
+		return (NULL);
+	if (!ft_import_xpm_img(var, var->txtr.NO_img.path, &var->txtr.NO_img))
+		return (NULL);
+	return (&var->txtr);
+}
+
+int	main(int argc, char **argv)
+{
+	t_var	var;
+
+	(void)argc;
+	// var.status = ft_init_var(&var, argv[1]);
+	// if (var.status == -1)
+	// 	return (ft_exit_error("Error\n"));
+	var.mlx_ptr = mlx_init();
+	mlx_get_screen_size(var.mlx_ptr, &(var.win.width), &(var.win.height));
+	var.win.title = ft_strdup(argv[1]);
+	var.win.win_ptr = mlx_new_window(var.mlx_ptr, var.win.width, var.win.height, var.win.title);
+	ft_import_xpm_img(&var, "./includes/textures/basalt_top.xpm", &(var.txtr.NO_img));
+	mlx_put_image_to_window(var.mlx_ptr, var.win.win_ptr, var.txtr.NO_img.data_addr, 100, 100);
+	mlx_loop(var.mlx_ptr);
 	return (0);
 }
